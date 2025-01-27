5545584debcd332a3c4bd9db29b3c761b1b6671e
ramura <44088736+rumra3@users.noreply.github.com>
2025-01-27 11:34:18 +0100

    install: add arg to --force overwrite symlinks

--- install.sh
+++ install.sh
@@ -2,17 +2,35 @@
 
 SRC=$(realpath $(dirname $BASH_SOURCE))
 
+force=""
 interactive=""
-if [ "$1" == "--interactive" ]; then
-    interactive="-p"
-fi
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -f|--force)
+            force="-f"
+            shift
+            ;;
+        -i|--interactive)
+            interactive="-p"
+            shift
+            ;;
+        -*|--*)
+            echo "unknown option $1"
+            exit 1
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
 
 function findhere() {
     find "$SRC" -mindepth 1 -maxdepth 1 "$@" -exec basename {} \;
 }
 
 findhere -type d -not -name ".*" |\
-    xargs $interactive -I % ln -sT "$SRC/%" "$HOME/.config/%"
+    xargs $interactive -I % ln $force -sT "$SRC/%" "$HOME/.config/%"
 
 findhere -type f -name ".*" -not -name ".gitignore" |\
-    xargs $interactive -I % ln -sT "$SRC/%" "$HOME/%"
+    xargs $interactive -I % ln $force -sT "$SRC/%" "$HOME/%"
